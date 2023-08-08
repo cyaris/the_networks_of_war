f60f65f997dffff14dca4db8ee5d10fbc5977b9e
Charles Yaris <charles.yaris.-nd@disney.com>
2023-08-07 23:25:11 -0500

    moved json files.

--- frontend/src/lib/components/App.svelte
+++ frontend/src/lib/components/App.svelte
@@ -5,16 +5,5 @@
   import { tooltip } from "svelte-lib/functions"
   import { Select, Text } from "svelte-lib/components"
 
-  const moduleNames = ['static/json/war_num_1_0.json']
-
-let modules = {}
-
-for (const moduleName of moduleNames) {
-    import module from './' + moduleName
-    modules.moduleName = module
-}
-
-// export modules
-console.log(modules)
-
+  const jsonDirectory = "assets/json"
 </script>
