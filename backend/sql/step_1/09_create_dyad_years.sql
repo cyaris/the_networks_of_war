b263d39015f2e792725602386b0d70c9f883c541
Charlie Yaris <charlieyaris@charlies-mbp.mynetworksettings.com>
2026-07-10 20:45:49 -0500

    added back quotes.

--- backend/sql/step_1/09_create_dyad_years.sql
+++ backend/sql/step_1/09_create_dyad_years.sql
@@ -11,7 +11,7 @@ select
     a.start_date_estimated,
     a.end_date_estimated,
     a.ongoing_war,
-    b.range::integer year
+    b.range::integer "year"
 from dyads a
 join range(1500, 2100) b on b.range between extract(year from a.start_date)::integer and extract(year from a.end_date)::integer
 group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11;
