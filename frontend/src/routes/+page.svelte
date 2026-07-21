98f9bc3ed6099b49049ece6281d1a7abd7473efb
Charlie Yaris <charlieyaris@charlies-mbp.mynetworksettings.com>
2026-07-20 22:27:18 -0500

    Fix mobile home buttons

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <div class="mb-8 flex flex-col items-center">
   <Button classes="w-56" href="tool" label="Begin Analysis" />
   <div class="mt-2 grid grid-cols-1 gap-x-1 gap-y-2 sm:grid-cols-2">
-    <Button classes="w-48" href="development" label="Development" />
-    <Button classes="w-52" href="documentation" label="Documentation" />
+    <Button classes="w-56" href="development" label="Development" />
+    <Button classes="w-56" href="documentation" label="Documentation" />
   </div>
 </div>
